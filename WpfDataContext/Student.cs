dda72db4fc42200bf966eeaed57446ad6dce0f34
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2019-12-20 08:40:44 +0300

    Add project files.

---
+++ WpfDataContext/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDataContext
+{
+	class Student
+	{
+		public string FirstName { get; set; }
+		public string SureName { get; set; }
+		public int Age { get; set; }
+	public string Departmant { get; set; }
+
+		public Student(string firstname,string surename,int age,string departmant) //constructor snippet is ctor
+		{
+			FirstName = firstname;
+			SureName = surename;
+			Age = age;
+			Departmant = departmant;
+		}
+
+	}
+}
